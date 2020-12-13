48fbd82cf52db61fcaebb075d40f5c4bf802876f
Bernt Isak Waerstad <berntisak@gmail.com>
2020-12-13 15:01:11 +0100

    Updated bash script and run parameters to model V4

--- run_cronjob.sh
+++
@@ -1,5 +0,0 @@
-rm ../DELPHI_local/danger_map/predicted/Parameters_Global_V2* -v
-yesterday=$(date -d '-1 days' +'%Y%m%d') 
-echo "$yesterday"
-wget https://raw.githubusercontent.com/COVIDAnalytics/website/master/data/predicted/Parameters_Global_V2_$yesterday.csv -O ../DELPHI_local/danger_map/predicted/Parameters_Global_V2_$yesterday.csv
-python DELPHI_model_V3.py --user grinder -o tnc -ci 0 -s100 0 -w 0
--- run_daily.sh
+++ run_daily.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 path=/home/pi/reverse-grinder/fortolkningsdissonans
 
-rm $path/DELPHI_local/danger_map/predicted/Parameters_Global_V2* -v
-yesterday=$(date -d '-1 days' +'%Y%m%d') 
+rm $path/DELPHI_local/danger_map/predicted/*
+
+yesterday=$(date -d '-1 days' +'%Y%m%d')
 echo "$yesterday"
 wget https://raw.githubusercontent.com/COVIDAnalytics/website/master/data/predicted/Parameters_Global_V4_$yesterday.csv -O $path/DELPHI_local/danger_map/predicted/Parameters_Global_V4_$yesterday.csv
 cd $path/DELPHI
-python $path/DELPHI/DELPHI_model_V4.py --user grinder -o tnc -ci 0 -s100 0 -w 0
+python $path/DELPHI/DELPHI_model_V4.py -rc run_configs/tnc-run-config.yml
